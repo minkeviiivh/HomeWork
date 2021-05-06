33a4e53eb5e3b18dc41ce6984c9810f698abbc27
alexw <minkeviiich@gmail.com>
2021-05-06 12:31:50 +0300

    Lesson 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+// Минкевич Александр (minkeviiich)
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
+
+
+
+
+
